50f26821559fe8a4bbc3e2e09d8a91f7617c0ffa
krishnapitike <50116547+krishnapitike@users.noreply.github.com>
2024-07-16 12:24:23 -0700

    Update rsync.sh
    use: ./rsync.sh u perl

--- rsync.sh
+++ rsync.sh
@@ -1,11 +1,15 @@
 perl=kcpitike@perlmutter.nersc.gov
-dec=piti526@deception.pnnl.gov
-remAcc=${perl}
-remDir=/pscratch/sd/k/kcpitike/V-alloy/configurations/
+dec=piti526@deception.pnl.gov
+remDir=/pscratch/sd/k/kcpitike/V-alloy/MLP/MLP-VH/
+#remdir=/qfs/projects/fusiondft/krishna/V-alloy/MLP/MLP-VH/
 locDir=.
 
+if [ ${2} == 'perl' ] ; then remAcc=${perl} ; elif [ ${2}=='dec' ] ; then remAcc=${dec} ; fi
+
+if [ ${1} == "d" ] ; then
+
 rsync -a -P --min-size=1 \
---max-size=1000m
+--max-size=1000m \
 --exclude='*.restart' \
 --exclude='CHG*' \
 --exclude='WAVECAR' \
@@ -14,9 +18,22 @@ rsync -a -P --min-size=1 \
 --exclude='vaspout.h5' \
 --exclude='log*' \
 --exclude='vasprun.xml' \
-if [ ${1} == "d" ] ; then 
-  ${remAcc}:${remDir}/${2} ${locDir}/
-  echo "Downloaded ${remAcc}:${remDir}/${2} to ${locDir}/${2}"
+${remAcc}:${remDir}/${3} ${locDir}/
+echo "Downloaded ${remAcc}:${remDir}/${3} to ${locDir}/${3}"
+
 elif [ ${1} == "u" ] ; then
-  ${locDir}/${2} ${remAcc}:${remDir}/
-  echo "Uploaded ${remAcc}:${remDir}/${2} to ${locDir}/${2}"
+
+rsync -a -P --min-size=1 \
+--max-size=1000m \
+--exclude='*.restart' \
+--exclude='CHG*' \
+--exclude='WAVECAR' \
+--exclude='*.err' \
+--exclude='*.out' \
+--exclude='vaspout.h5' \
+--exclude='log*' \
+--exclude='vasprun.xml' \
+${locDir}/${3} ${remAcc}:${remDir}/
+echo "Uploaded ${locDir}/${3} ${remAcc}:${remDir}/${3}"
+
+fi
