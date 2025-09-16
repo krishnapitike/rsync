40965f6971f026301f076e8b3067711e64d2528a
krishnapitike <50116547+krishnapitike@users.noreply.github.com>
2025-09-16 15:21:44 -0700

    Update remote directories and rsync logic

--- rsync.sh
+++ rsync.sh
@@ -1,11 +1,22 @@
 perl=kcpitike@perlmutter.nersc.gov
-dec=piti526@deception.pnl.gov
+dec=piti526@dec-admin9.pnl.gov
 kest=kcpitike@kestrel.nrel.gov
-remDir=/pscratch/sd/k/kcpitike/V-alloy/MLP/MLP-VH/
-#remdir=/qfs/projects/fusiondft/krishna/V-alloy/MLP/MLP-VH/
+pl2=piti526@pleiades2.pnl.gov
+remDirDec=/rcfs/projects/fusiondft/krishna/MPEA/dislocations/nep/barrier/master/sites_212/
+remDirPL2=/files4/krishna/mpea_dislocation/barrier/master/sites_212
 locDir=.
 
-if [ ${2} == 'perl' ] ; then remAcc=${perl} ; elif [ ${2}=='dec' ] ; then remAcc=${dec} ; fi
+if [ ${2} == 'perl' ] ; then
+  remAcc=${perl}
+  remDir=${remDirPerl}
+elif [ ${2} == 'dec' ] ; then
+  remAcc=${dec}
+  remDir=${remDirDec}
+elif [ ${2} == 'pl2' ] ; then
+  remAcc=${pl2}
+  remDir=${remDirPL2}
+  export RSYNC_RSH="ssh -o HostKeyAlgorithms=+ssh-rsa -o PubkeyAcceptedAlgorithms=+ssh-rsa"
+fi
 
 if [ ${1} == "d" ] ; then
 
@@ -14,10 +25,7 @@ rsync -a -P --min-size=1 \
 --exclude='*.restart' \
 --exclude='CHG*' \
 --exclude='WAVECAR' \
---exclude='*.err' \
---exclude='*.out' \
 --exclude='vaspout.h5' \
---exclude='log*' \
 --exclude='vasprun.xml' \
 ${remAcc}:${remDir}/${3} ${locDir}/
 echo "Downloaded ${remAcc}:${remDir}/${3} to ${locDir}/${3}"
@@ -29,10 +37,7 @@ rsync -a -P --min-size=1 \
 --exclude='*.restart' \
 --exclude='CHG*' \
 --exclude='WAVECAR' \
---exclude='*.err' \
---exclude='*.out' \
 --exclude='vaspout.h5' \
---exclude='log*' \
 --exclude='vasprun.xml' \
 ${locDir}/${3} ${remAcc}:${remDir}/
 echo "Uploaded ${locDir}/${3} ${remAcc}:${remDir}/${3}"
